49e9d9e36fe07c28ada7fb804cf58c1737f802e1
Raphael-Alexei <dimagameryt@gmail.com>
2022-04-28 22:37:13 -0300

    Updated preDeath event

--- src/main/java/com/github/idimabr/listeners/EntityDamageListener.java
+++ src/main/java/com/github/idimabr/listeners/EntityDamageListener.java
@@ -3,26 +3,30 @@ package com.github.idimabr.listeners;
 import com.github.idimabr.customevents.EntityPreDeathEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.metadata.FixedMetadataValue;
 
 public class EntityDamageListener implements Listener {
 
     @EventHandler
-    public void onDamage(EntityDamageEvent e){
-        Entity entity = e.getEntity();
-        EntityDamageEvent.DamageCause cause = e.getCause();
-        double damage = e.getDamage();
-        if(!(entity instanceof LivingEntity)) return;
+    public void onDamage(EntityDamageByEntityEvent e){
+        if(!(e.getEntity() instanceof LivingEntity)) return;
 
-        LivingEntity livingEntity = (LivingEntity) entity;
+        LivingEntity entity = (LivingEntity) e.getEntity();
+        if(!entity.hasMetadata("stacked")) return;
+        if(entity.getMetadata("stacked").size() == 0) return;
 
-        if(livingEntity.getHealth() - damage <= 0) {
+        if( (entity.getHealth() - (e.getDamage())) <= 0){
             e.setCancelled(true);
             e.setDamage(0);
 
+            EntityDamageEvent.DamageCause cause = e.getCause();
             EntityPreDeathEvent preDeath = new EntityPreDeathEvent(entity, cause);
             Bukkit.getServer().getPluginManager().callEvent(preDeath);
         }
